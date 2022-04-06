ed547a1e72913c568091e2a9e81f1755035c602d
Jared Gonsalves446 <JGonsalves446@students.ccsnh.edu>
2022-04-06 10:22:32 -0400

    pushing up ui design change

--- Assessments/UIForm.h
+++ Assessments/UIForm.h
@@ -34,6 +34,9 @@ namespace Assessments {
 		// Auto generated code
 	private: System::Windows::Forms::Button^ enterBtn;
 	private: System::Windows::Forms::TextBox^ authKey;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
 
 	private:
 		// Required designer variable.
@@ -47,15 +50,20 @@ namespace Assessments {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(UIForm::typeid));
 			this->enterBtn = (gcnew System::Windows::Forms::Button());
 			this->authKey = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
 			this->SuspendLayout();
 			// 
 			// enterBtn
 			// 
-			this->enterBtn->Location = System::Drawing::Point(195, 12);
+			this->enterBtn->Location = System::Drawing::Point(215, 81);
+			this->enterBtn->Margin = System::Windows::Forms::Padding(2);
 			this->enterBtn->Name = L"enterBtn";
-			this->enterBtn->Size = System::Drawing::Size(75, 23);
+			this->enterBtn->Size = System::Drawing::Size(56, 19);
 			this->enterBtn->TabIndex = 0;
 			this->enterBtn->Text = L"Enter";
 			this->enterBtn->UseVisualStyleBackColor = true;
@@ -63,20 +71,59 @@ namespace Assessments {
 			// 
 			// authKey
 			// 
-			this->authKey->Location = System::Drawing::Point(12, 12);
+			this->authKey->Location = System::Drawing::Point(77, 81);
+			this->authKey->Margin = System::Windows::Forms::Padding(2);
 			this->authKey->Name = L"authKey";
-			this->authKey->Size = System::Drawing::Size(177, 22);
+			this->authKey->Size = System::Drawing::Size(134, 20);
 			this->authKey->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(24, 84);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(48, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"API Key:";
+			this->label1->Click += gcnew System::EventHandler(this, &UIForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(20, 106);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(51, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Class List";
+			this->label2->Click += gcnew System::EventHandler(this, &UIForm::label2_Click);
+			// 
+			// checkedListBox1
+			// 
+			this->checkedListBox1->FormattingEnabled = true;
+			this->checkedListBox1->IntegralHeight = false;
+			this->checkedListBox1->Location = System::Drawing::Point(77, 106);
+			this->checkedListBox1->Name = L"checkedListBox1";
+			this->checkedListBox1->Size = System::Drawing::Size(120, 96);
+			this->checkedListBox1->TabIndex = 4;
 			// 
 			// UIForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->ClientSize = System::Drawing::Size(650, 463);
+			this->Controls->Add(this->checkedListBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->authKey);
 			this->Controls->Add(this->enterBtn);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"UIForm";
 			this->Text = L"UIForm";
+			this->Load += gcnew System::EventHandler(this, &UIForm::UIForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -85,5 +132,11 @@ namespace Assessments {
 
 		// Auto generated code by double clicking button in UIForm
 	private: System::Void enterBtn_Click(System::Object^ sender, System::EventArgs^ e);
-	};
+	private: System::Void UIForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
