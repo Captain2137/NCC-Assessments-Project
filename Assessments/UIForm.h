e186e57d086e4d77a450b0c78281a998ca3abcdd
Jared Gonsalves446 <JGonsalves446@students.ccsnh.edu>
2022-04-11 10:42:06 -0400

    second form changes

--- Assessments/UIForm.h
+++ Assessments/UIForm.h
@@ -60,10 +60,10 @@ namespace Assessments {
 			// 
 			this->enterBtn->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->enterBtn->Location = System::Drawing::Point(603, 351);
+			this->enterBtn->Location = System::Drawing::Point(452, 268);
 			this->enterBtn->Margin = System::Windows::Forms::Padding(2);
 			this->enterBtn->Name = L"enterBtn";
-			this->enterBtn->Size = System::Drawing::Size(149, 50);
+			this->enterBtn->Size = System::Drawing::Size(112, 38);
 			this->enterBtn->TabIndex = 0;
 			this->enterBtn->Text = L"Enter";
 			this->enterBtn->UseVisualStyleBackColor = true;
@@ -73,10 +73,10 @@ namespace Assessments {
 			// 
 			this->authKey->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->authKey->Location = System::Drawing::Point(278, 348);
+			this->authKey->Location = System::Drawing::Point(208, 266);
 			this->authKey->Margin = System::Windows::Forms::Padding(2);
 			this->authKey->Name = L"authKey";
-			this->authKey->Size = System::Drawing::Size(321, 53);
+			this->authKey->Size = System::Drawing::Size(242, 44);
 			this->authKey->TabIndex = 1;
 			this->authKey->TextChanged += gcnew System::EventHandler(this, &UIForm::authKey_TextChanged);
 			// 
@@ -85,22 +85,23 @@ namespace Assessments {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(99, 350);
-			this->label1->MinimumSize = System::Drawing::Size(149, 50);
+			this->label1->Location = System::Drawing::Point(65, 269);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label1->MinimumSize = System::Drawing::Size(112, 38);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(174, 50);
+			this->label1->Size = System::Drawing::Size(139, 38);
 			this->label1->TabIndex = 2;
 			this->label1->Text = L"API Key:";
 			this->label1->Click += gcnew System::EventHandler(this, &UIForm::label1_Click);
 			// 
 			// UIForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 17);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ButtonHighlight;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
-			this->ClientSize = System::Drawing::Size(859, 489);
+			this->ClientSize = System::Drawing::Size(644, 374);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->authKey);
 			this->Controls->Add(this->enterBtn);
--- Assessments/UISelectCourses.h
+++ Assessments/UISelectCourses.h
@@ -31,6 +31,15 @@ namespace Assessments {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::ComboBox^ comboBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	protected:
 
 	private:
 		/// <summary>
@@ -45,20 +54,108 @@ namespace Assessments {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// checkedListBox1
+			// 
+			this->checkedListBox1->FormattingEnabled = true;
+			this->checkedListBox1->Location = System::Drawing::Point(133, 327);
+			this->checkedListBox1->Name = L"checkedListBox1";
+			this->checkedListBox1->Size = System::Drawing::Size(605, 199);
+			this->checkedListBox1->TabIndex = 0;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(342, 192);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(214, 21);
+			this->comboBox1->TabIndex = 2;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Location = System::Drawing::Point(342, 219);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(214, 21);
+			this->comboBox2->TabIndex = 3;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(342, 246);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(214, 20);
+			this->textBox1->TabIndex = 4;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(263, 195);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(73, 13);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Add Instructor";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(263, 222);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(73, 13);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Add Semester";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(234, 249);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(102, 13);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Add Course Number";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(130, 311);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(82, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Added Courses:";
 			// 
 			// UISelectCourses
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(493, 393);
+			this->ClientSize = System::Drawing::Size(881, 538);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->checkedListBox1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"UISelectCourses";
 			this->Text = L"UISelectCourses";
+			this->Load += gcnew System::EventHandler(this, &UISelectCourses::UISelectCourses_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 		// Auto generated code by double clicking button in UIForm
 	private: System::Void enterBtn_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void UISelectCourses_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
