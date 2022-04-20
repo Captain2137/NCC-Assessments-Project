7d7ab47e2e96cab6d4bffa0f00d789e5bd21815a
SAMANTHA HUGHSON <shughson127@students.ccsnh.edu>
2022-04-20 10:43:41 -0400

    Created an if statement to end application if user enters blank string, created shell functions for button clicks - Samantha

--- Assessments/UIForm.cpp
+++ Assessments/UIForm.cpp
@@ -9,19 +9,25 @@ using namespace util;
 // On enter button click, get authorization key from authKey, and exit form
 System::Void Assessments::UIForm::enterBtn_Click(System::Object^ sender, System::EventArgs^ e) {
 	// Set auth as what user put in textBox1
-	*auth = msclr::interop::marshal_as<std::string>(authKey->Text);
+	if (authKey->Text == "") {
+		Application::Exit();
+	}
+	else {
+		*auth = msclr::interop::marshal_as<std::string>(authKey->Text);
 
-	// Get data from online server
-	nlohmann::json j = nlohmann::json::parse(util::curlRequest("https://canvas.nashuaweb.net/api/v1/users/self?access_token=" + *auth));
-	// Change canvas.nashuaweb.net to canvas-prod.ccsnh.edu in final
+		// Get data from online server
+		nlohmann::json j = nlohmann::json::parse(util::curlRequest("https://canvas.nashuaweb.net/api/v1/users/self?access_token=" + *auth));
+		// Change canvas.nashuaweb.net to canvas-prod.ccsnh.edu in final
 
-	// Debug: Print raw json with formating
-	std::cout << "Raw Json:\n" << j.dump(4) << std::endl << std::endl;
+		// Debug: Print raw json with formating
+		std::cout << "Raw Json:\n" << j.dump(4) << std::endl << std::endl;
 
-	if (j.contains("errors")) {	// If error, print error message(s)
-		std::cout << "Error: " << j["errors"][0]["message"].get<std::string>() << std::endl << std::endl;
-	} else {	// If success, get user's name
-		*userName = j["name"];
+		if (j.contains("errors")) {	// If error, print error message(s)
+			std::cout << "Error: " << j["errors"][0]["message"].get<std::string>() << std::endl << std::endl;
+		}
+		else {	// If success, get user's name
+			*userName = j["name"];
+		}
 	}
 
 	// Close form
