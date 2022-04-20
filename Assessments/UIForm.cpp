0bd329021991244138f4a86229c5a89a87ad50de
Jacob Briand <terryturtletime@outlook.com>
2022-04-20 15:29:24 -0400

    Added Comments

--- Assessments/UIForm.cpp
+++ Assessments/UIForm.cpp
@@ -10,7 +10,7 @@ System::Void Assessments::UIForm::enterBtn_Click(System::Object^ sender, System:
 	// Set auth as what user put in textBox1
 	if (authKey->Text == "") {
 		MessageBox::Show(L"Error: No Canvas Key was Entered. The Application will now Exit."); // Message Box Alerting the User that Nothing was Inputted.
-		Application::Exit();
+		Application::Exit();	// This Exits the Application Entirely
 	}
 
 	*auth = msclr::interop::marshal_as<std::string>(authKey->Text);
@@ -22,11 +22,14 @@ System::Void Assessments::UIForm::enterBtn_Click(System::Object^ sender, System:
 	// Debug: Print raw json with formating
 	std::cout << "Raw Json:\n" << j.dump(4) << std::endl << std::endl;
 
+	// If Statement Checks Whether the User Inputted a Valid Canvas API Key or Not
+	// If the Json Contains Errors, Print the Error to the Console, Inform the User and Exit the Application
 	if (j.contains("errors")) {	// If error, print error message(s), exits program
 		std::cout << "Error: " << j["errors"][0]["message"].get<std::string>() << std::endl << std::endl;
 		MessageBox::Show(L"Error: The Canvas Key Inputted is Incorrect.");	// Message Box Alerting the User that an Incorrect API was Inputted.
-		Application::Exit();
+		Application::Exit();	// This Exits the Application Entirely
 	}
+	// Else the API Key did not Contain any Errors, the User's Username is Returned.  
 	else {	// If success, get user's name
 		*userName = j["name"];
 	}
