9a83e7fe1af6c3abd48db04274ddfde4b1389a9e
NCC Lab User <NCC Lab User@Nash-Stu.nhctc.edu>
2022-04-20 10:25:05 -0400

    Moved couple things in util - Mitchell

--- Assessments/Util.h
+++ Assessments/Util.h
@@ -5,4 +5,11 @@
 namespace util {
     std::string curlRequest(std::string url);   // Get data from online servers from given URL
     std::vector<std::vector<double>> calcData(std::vector<std::vector<int>>);   // Calculates the course data
+
+    /*
+    double calcAvg(std::vector<int>);		// Calculates the average of a vector of doubles
+    double calcMedian(std::vector<int>);    // Calculates the median of a vector of doubles
+    double calcPercent(std::vector<int>);    // Calculates the percent above three of a vector of doubles
+    double calcDeviation(std::vector<int>);  // Calculates the standard deviation of a vector of doubles
+    */
 }
