cef2f45306ffd54685827cee46a42e45529d3237
NCC Lab User <NCC Lab User@Nash-Stu.nhctc.edu>
2022-04-25 10:47:34 -0400

    Fixed median and cleaned code in util - Mitchell

--- Assessments/Util.cpp
+++ Assessments/Util.cpp
@@ -46,12 +46,6 @@ std::string util::curlRequest(std::string url) {
 // Calculates the averages, medians, percents above three, and 
 // standard devations in each competency given a 2D vector of courses data
 std::vector<std::vector<double>> util::calcData(std::vector<std::vector<double>> courseData) {
-	
-	/*
-	// The number of competencies in the courses
-	int compSize = (int)courseData[0].size();
-	*/
-	
 	// The number of students in the course
 	int classSize = (int)courseData.size();
 	// The largest number of comps in a course
@@ -83,27 +77,6 @@ std::vector<std::vector<double>> util::calcData(std::vector<std::vector<double>>
 			compData[j].push_back(courseData[i][j]);
 		}
 	}
-
-	/*
-	// For all comps in the course
-	for (int i = 0; i < compSize; i++) {
-		// For all students in the comp
-		for (int j = 0; j < classSize; j++) {
-			// Add the current student's data for the current comp to the 2D vector
-			compData[i].push_back(courseData[j][i]);
-		}
-	}*/
-
-	/*
-	// For all comps in the vector
-	for (int i = 0; i < largestCompSize; i++) {
-		// Add the average, median, percent above three, and standard deviation
-		// for the current comp to the vector of calculated data
-		calcedData[0].push_back(calcAvg(compData[i]));
-		calcedData[1].push_back(calcMedian(compData[i]));
-		calcedData[2].push_back(calcPercent(compData[i]));
-		calcedData[3].push_back(calcDeviation(compData[i]));
-	}*/
 
 	// For all comps in the vector
 	for (int i = 0; i < largestCompSize; i++) {
@@ -141,7 +114,7 @@ std::vector<std::vector<double>> util::calcData(std::vector<std::vector<double>>
 		}
 
 		// If the number of scores is even
-		if (middle % 2 == 0) {
+		if (size % 2 == 0) {
 			// Calculate the median score and add to the vector
 			calcedData[1].push_back((compData[i][middle] + compData[i][middle - 1]) / 2);
 		}
@@ -162,70 +135,3 @@ std::vector<std::vector<double>> util::calcData(std::vector<std::vector<double>>
 	// Return the calced data
 	return calcedData;
 }
-
-/*
-// Calculates the average given a vector of doubles
-double calcAvg(std::vector<int> scores) {
-	int size = (int)scores.size();	// The number of scores in the vector
-	double sum = 0;					// The sum of the scores
-
-	// For all scores in the vector
-	for (int i = 0; i < size; i++) {
-		sum += scores[i];
-	}
-
-	// Calculate and return the average score
-	return sum / size;
-}
-
-// Calculates the median given a vector of doubles
-double calcMedian(std::vector<int> scores) {
-	int middle = (int)scores.size() / 2;		// The middle score if odd number of scores
-	std::sort(scores.begin(), scores.end());	// Sort the scores in ascending order
-
-	// If the number of scores is even
-	if (middle % 2 == 0) {
-		// Calculate the median score
-		return (scores[middle] + scores[middle - 1]) / 2.0;
-	}
-
-	// If the number of scores is odd
-	else {
-		// The middle score is the median score
-		return scores[middle];
-	}
-}
-
-// Calculates the number of scores tha are 3 or higher given a vector of doubles
-double calcPercent(std::vector<int> scores) {
-	int size = (int)scores.size();	// The number of scores in the vector
-	int counter = 0;				// The number of scores that are 3 or higher
-
-	// For all scores in the vector
-	for (int i = 0; i < size; i++) {
-		// If the score is 3 or higher
-		if (scores[i] >= 3) {
-			// Add the score to the counter
-			counter++;
-		}
-	}
-	// Calculate and return the percent of scores that are 3 or higher
-	return counter * 100.0 / size;
-}
-
-// Calculates the standard deviation given a vector of doubles
-double calcDeviation(std::vector<int> scores) {
-	int size = (int)scores.size();		// The number of scores in the vector
-	double average = calcAvg(scores);	// Use the calcAvg function to get the average of the scores
-	double sum = 0;						// The sum of the squares of each deviation
-
-	// For all scores in the vector
-	for (int i = 0; i < size; i++) {
-		// Calculate the deviation from the average,
-		// square it, and add the result to the sum
-		sum += pow((scores[i] - average), 2);
-	}
-
-	// Calculate and return the standard deviation
-	return sqrt((double)sum / (size - 1));
-}*/
